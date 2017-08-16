58ff2cc5401dcd422c4c87f539075e5415132ecd
MarkLux <marlx6590@163.com>
2017-08-16 15:43:57 +0800

    remove test codes

--- main.go
+++ main.go
@@ -1,10 +1,6 @@
 package main
 
 import (
-	"fmt"
-
-	"github.com/MarkLux/JudgeServer/client"
-	"github.com/MarkLux/JudgeServer/config"
 	"github.com/MarkLux/JudgeServer/server"
 	"github.com/gin-gonic/gin"
 )
@@ -16,35 +12,5 @@ func main() {
 
 	r.POST("/judge", server.Judge)
 
-	r.Run(":8070")
-
-	// test()
-}
-
-func test() {
-
-	conf := config.CompileC.RunConfig
-
-	conf.Command.FillWith(map[string]string{
-		"{exe_path}": "/home/judge/output/main",
-	})
-
-	jc := client.JudgeClient{
-		MaxCpuTime:    1000,
-		MaxMemory:     128 * 1024 * 1024,
-		RunConf:       conf,
-		TestCaseId:    1001,
-		SubmissionDir: "/home/judge/user/1001",
-	}
-
-	rs, _ := jc.Judge()
-
-	for _, p := range rs.Passed {
-		fmt.Println("passed: ")
-		fmt.Printf("%#v", p)
-	}
-	for _, u := range rs.UnPassed {
-		fmt.Println("unpassed: ")
-		fmt.Printf("%#v", u)
-	}
+	r.Run(":8090")
 }
