4f4821cd7d4fb82f492ec750fc9e0a707077d788
MarkLux <marlx6590@163.com>
2017-10-17 20:25:00 +0800

    fix bug of rsync service

--- rsync/rsync.go
+++ rsync/rsync.go
@@ -12,7 +12,7 @@ import (
 func SyncSingle(testCaseId string) (err error) {
 	remote := os.Getenv("RSYNC_USER") + "@" + os.Getenv("RSYNC_HOST")
 	rsyncPwd := filepath.Join(config.TEST_CASE_DIR, "rsync.passwd")
-	rsyncCmd := `rsync -av --password-file ` + rsyncPwd + ` --include="*.in" --include="*.out"  --include="*/"  --exclude="*" ` + remote + `::testcases/` + testCaseId + ` ` + config.TEST_CASE_DIR
+	rsyncCmd := `rsync -av --password-file ` + rsyncPwd + ` --include="*.in" --include="*.out"  --include="*/"  --exclude="*" --delete` + remote + `::testcases/` + testCaseId + ` ` + config.TEST_CASE_DIR
 	in := bytes.NewBuffer(nil)
 	cmd := exec.Command("/bin/bash")
 	cmd.Stdin = in
